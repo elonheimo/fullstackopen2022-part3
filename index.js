50752be41ad504b312dd11560290e7803009e279
Johannes Elonheimo <johannes.elonheimo@gmail.com>
2022-08-31 13:43:13 +0300

    uibuild

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get('/api/persons/:id', (req, res, next) => {
 
 app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndRemove(req.params.id)
-        .then(res => {
+        .then(() => {
             res.status(204).end()
         })
         .catch(error => next(error))
